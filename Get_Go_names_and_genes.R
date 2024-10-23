eda1e7274d71b06b8b4fa2fa9e658baf4f702a07
DatViseR <dzdzstepel@gmail.com>
2024-10-23 15:58:10 +0200

    created subset of annotation data to parse

--- Get_Go_names_and_genes.R
+++ Get_Go_names_and_genes.R
@@ -23,23 +23,26 @@ library(GSEABase)
 # Load the GO annotations
 go_obo <- getOBOCollection("go-basic.obo")
 
+GO_non_parsed <- go_obo@.kv
+
+GO_non_parsed_subset <- GO_non_parsed[1:2000, ]
 
 #extracting the GO numbers with names https://stackoverflow.com/questions/78532080/getting-namespace-from-a-obo-formatted-text-file
 
-dat <- read.delim("go-basic.obo", header=F)
+# dat <- read.delim("go-basic.obo", header=F)
 
-#Getting a data frame format from non-repeated terms
-
-
- GO_numbers_with_names <-  do.call(rbind, 
-          apply(cbind(grep("\\[Term]", dat$V1) + 1, 
-                      grep("\\[Term]", dat$V1) - 1 + 
-                        diff(c(grep("\\[Term]", dat$V1), nrow(dat)))), 1, \(x){
-                          res <- data.table::tstrsplit(dat[x[1]:x[2],], ": ")
-                          id <- grep("^id|name|namespace|def|is_obso", res[[1]])
-                          `length<-`(as.list(res[[2]][id]), 5)})) |> 
-    data.frame() |> 
-    setNames(c("id", "name", "namespace", "definition", "is_obsolete"))
+# #Getting a data frame format from non-repeated terms
+# 
+# 
+#  GO_numbers_with_names <-  do.call(rbind, 
+#           apply(cbind(grep("\\[Term]", dat$V1) + 1, 
+#                       grep("\\[Term]", dat$V1) - 1 + 
+#                         diff(c(grep("\\[Term]", dat$V1), nrow(dat)))), 1, \(x){
+#                           res <- data.table::tstrsplit(dat[x[1]:x[2],], ": ")
+#                           id <- grep("^id|name|namespace|def|is_obso", res[[1]])
+#                           `length<-`(as.list(res[[2]][id]), 5)})) |> 
+#     data.frame() |> 
+#     setNames(c("id", "name", "namespace", "definition", "is_obsolete"))
 
 
 
