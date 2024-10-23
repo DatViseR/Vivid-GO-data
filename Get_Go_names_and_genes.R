319f5438e53099bcd77e066d0c1af92420c75e03
DatViseR <dzdzstepel@gmail.com>
2024-10-23 18:06:36 +0200

    Created final txt file with gene id, name, gene, ontology

--- Get_Go_names_and_genes.R
+++ Get_Go_names_and_genes.R
@@ -28,40 +28,36 @@ GO_non_parsed <- go_obo@.kv
 GO_non_parsed_subset <- GO_non_parsed[1:2000, ]
 
 #write GO_non_parsed_subset to a file
-write.table(GO_non_parsed_subset, "GO_non_parsed_subset.txt", sep = "\t", row.names = FALSE, col.names = FALSE)
+write.table(GO_non_parsed,"GO_non_parsed.txt", sep = "\t", row.names = FALSE, col.names = FALSE)
 
 # filter the GO_non_parsed_subset for name
-GO_numbers_with_names <- GO_non_parsed_subset[grepl("name", GO_non_parsed_subset$key), ]
+GO_numbers_with_names <- GO_non_parsed[grepl("name", GO_non_parsed$key), ]
 
-#put the key "namespace" in its own column using the pivot family functions
+# move the values in key column into separate columns
+GO_numbers_with_names2 <- GO_numbers_with_names |>
+  tidyr::pivot_wider(names_from = key, values_from = value)
 
 
-
-
-
+#remove first raw
+GO_numbers_with_names2 <- GO_numbers_with_names2[-1, ] 
+#remove second column
+GO_numbers_with_names2 <- GO_numbers_with_names2[, -2]
 
 # View the result
-print(GO_numbers_with_names)
+print(GO_numbers_with_names2)
 
+# change column names to "id", "name", "ontology"
+GO_numbers_with_names2 <- setNames(GO_numbers_with_names2, c("id", "name", "ontology"))
 
+#write GO_numbers_with_names2 to a file
+write.table(GO_numbers_with_names2,"GO_numbers_with_names2.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 
+#change the colnames of gaf_filtered to match the GO_numbers_with_names2
+colnames(gaf_filtered) <- c("id", "gene", "ontology")
 
-#extracting the GO numbers with names https://stackoverflow.com/questions/78532080/getting-namespace-from-a-obo-formatted-text-file
+# join the gaf_filtered and GO_numbers_with_names2 on id
+GO_names_and_genes <- dplyr::left_join(gaf_filtered, GO_numbers_with_names2, by = "id")
 
-# dat <- read.delim("go-basic.obo", header=F)
-
-# #Getting a data frame format from non-repeated terms
-# 
-# 
-#  GO_numbers_with_names <-  do.call(rbind, 
-#           apply(cbind(grep("\\[Term]", dat$V1) + 1, 
-#                       grep("\\[Term]", dat$V1) - 1 + 
-#                         diff(c(grep("\\[Term]", dat$V1), nrow(dat)))), 1, \(x){
-#                           res <- data.table::tstrsplit(dat[x[1]:x[2],], ": ")
-#                           id <- grep("^id|name|namespace|def|is_obso", res[[1]])
-#                           `length<-`(as.list(res[[2]][id]), 5)})) |> 
-#     data.frame() |> 
-#     setNames(c("id", "name", "namespace", "definition", "is_obsolete"))
-
-
+#save GO_names_and_genes to a file
+write.table(GO_names_and_genes,"GO_names_and_genes.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 
