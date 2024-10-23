6f419993b5c0f88b9edac85244b67c64573ad846
DatViseR <dzdzstepel@gmail.com>
2024-10-23 18:11:37 +0200

    Created automated header summary for th Re script

--- Get_Go_names_and_genes.R
+++ Get_Go_names_and_genes.R
@@ -1,3 +1,33 @@
+# # This script create a datatable with all the GO annotations matched to genes and 
+# This script performs the following tasks:
+#   
+#   Load Gene Ontology Annotation (GAF) File:
+#   Reads the GAF file goa_human.gaf and filters relevant columns (GO_ID, Gene, Aspect).
+# Splits the filtered data into biological process, molecular function, and cellular component aspects.
+# 
+# Install and Load Required Packages:
+#   Installs and loads the BiocManager and GSEABase packages.
+# Loads GO annotations from the go-basic.obo file into GO_non_parsed.
+# 
+# Process GO Annotations:
+#   Extracts a subset of GO_non_parsed and writes it to GO_non_parsed.txt.
+# Filters for rows containing "name" and pivots the data to create GO_numbers_with_names2.
+# 
+# Clean and Prepare Data:
+#   Removes the first row and second column from GO_numbers_with_names2.
+# Renames columns to "id", "name", and "ontology".
+# Writes GO_numbers_with_names2 to GO_numbers_with_names2.txt.
+# 
+# Join GO Data with GAF Data:
+#   Renames columns of gaf_filtered to match GO_numbers_with_names2.
+# Joins gaf_filtered with GO_numbers_with_names2 on "id" to create GO_names_and_genes.
+# Writes GO_names_and_genes to GO_names_and_genes.txt.
+# 
+# This script processes and combines Gene Ontology (GO) data with gene annotation data to produce comprehensive tables of GO terms and their associated genes.
+
+
+
+
 #Download gene ontology annotation 
 library(data.table)
 # Load GAF file
@@ -5,12 +35,6 @@ gaf_file <- "goa_human.gaf"  # Ensure you've downloaded the file to this path
 gaf <- fread(gaf_file, skip = "!")  # Skip header lines starting with '!'
 # Select the relevant columns
 gaf_filtered <- gaf[, .(GO_ID = V5, Gene = V3, Aspect = V9)]
-
-# Split the data into different aspects
-biological_process <- gaf_filtered[Aspect == "P"]
-molecular_function <- gaf_filtered[Aspect == "F"]
-cellular_component <- gaf_filtered[Aspect == "C"]
-
 
 # Install required packages if not already installed
 if (!require("BiocManager", quietly = TRUE))
@@ -57,6 +81,8 @@ colnames(gaf_filtered) <- c("id", "gene", "ontology")
 
 # join the gaf_filtered and GO_numbers_with_names2 on id
 GO_names_and_genes <- dplyr::left_join(gaf_filtered, GO_numbers_with_names2, by = "id")
+
+GO_names_and_genes<-GO_names_and_genes |> dplyr::select(id,name,gene, ontology = ontology.x)
 
 #save GO_names_and_genes to a file
 write.table(GO_names_and_genes,"GO_names_and_genes.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
