83daf5d03f33da7b6c2f9ddd7727a0b97669e2e4
DatViseR <dzdzstepel@gmail.com>
2024-12-12 11:57:38 +0100

    The script has been modified - the obsolete terms were removed from the obo file and the filter for at least 6 annotation for a given GO term was set up

--- Get_Go_names_and_genes2.R
+++ Get_Go_names_and_genes2.R
@@ -15,29 +15,31 @@
 # 
 # Clean and Prepare Data:
 #   Removes the first row and second column from GO_numbers_with_names2.
+# remove the obsolete GO terms from the obo file 
 # Renames columns to "id", "name", and "ontology".
 # Writes GO_numbers_with_names2 to GO_numbers_with_names2.txt.
 # 
 # Join GO Data with GAF Data:
+# filters the gaf file to remove the annotations with obsolete terms 
 #   Renames columns of gaf_filtered to match GO_numbers_with_names2.
 # Joins gaf_filtered with GO_numbers_with_names2 on "id" to create GO_names_and_genes.
 # Writes GO_names_and_genes to GO_names_and_genes.txt.
+# Leaves the GO terms that have at least 6 gene annotations present
+
 # 
 # This script processes and combines Gene Ontology (GO) data with gene annotation data to produce comprehensive tables of GO terms and their associated genes.
 
 
-
+library(dplyr)
 
 #Download gene ontology annotation 
 library(data.table)
-# Load GAF file
+# Load GAF file 2024-11-03 release
+#EBI Gene Ontology Annotation Database (goa) 	protein 	782871 	goa_human.gaf (gzip)
 gaf_file <- "goa_human2.gaf"  # Ensure you've downloaded the file to this path
 gaf <- fread(gaf_file, skip = "!")  # Skip header lines starting with '!'
-# Select the relevant columns - filter pseudogenes out of the data
+# Select the relevant columns - filter pseudogenes out of the data # Filter for protein-coding genes
 gaf_filtered <- gaf %>% filter(V12 == "protein") %>% select(V3, V5, V9) 
-
-
-# Filter for protein-coding genes
 
 
 # Install required packages if not already installed
@@ -49,9 +51,25 @@ BiocManager::install("GSEABase")
 library(GSEABase)
 
 # Load the GO annotations
-go_obo <- getOBOCollection("go-basic.obo")
+go_obo <- getOBOCollection("go-basic2.obo")
 
 GO_non_parsed <- go_obo@.kv
+
+# Extract obsolete terms
+obsolete_terms <- GO_non_parsed[GO_non_parsed$key == "is_obsolete" & GO_non_parsed$value == "true", ]
+
+# Filter out rows with obsolete GO IDs from the gaf_filtered data
+gaf_filtered <- gaf_filtered[!gaf_filtered$V5 %in% obsolete_terms$stanza_id]
+
+# there are no obsolete terms in the gaf file
+table(!gaf_filtered$V5 %in% obsolete_terms$stanza_id)
+
+# leave the GO terms that have at least 4 gene annotations present in the gaf file
+gaf_filtered <- gaf_filtered %>%
+  group_by(V5) %>%
+  filter(n() >= 6) %>%
+  ungroup()
+
 
 GO_non_parsed_subset <- GO_non_parsed[1:2000, ]
 
@@ -62,31 +80,43 @@ write.table(GO_non_parsed,"GO_non_parsed.txt", sep = "\t", row.names = FALSE, co
 GO_numbers_with_names <- GO_non_parsed[grepl("name", GO_non_parsed$key), ]
 
 # move the values in key column into separate columns
-GO_numbers_with_names2 <- GO_numbers_with_names |>
+GO_numbers_with_names <- GO_numbers_with_names |>
   tidyr::pivot_wider(names_from = key, values_from = value)
 
 
 #remove first raw
-GO_numbers_with_names2 <- GO_numbers_with_names2[-1, ] 
+GO_numbers_with_names <- GO_numbers_with_names2[-1, ] 
 #remove second column
-GO_numbers_with_names2 <- GO_numbers_with_names2[, -2]
+GO_numbers_with_names <- GO_numbers_with_names2[, -2]
+
+# removes the hits with "obsolete" in the name
+GO_numbers_with_names <- GO_numbers_with_names2[!grepl("obsolete", GO_numbers_with_names2$name), ]
+
 
 # View the result
 print(GO_numbers_with_names2)
 
 # change column names to "id", "name", "ontology"
-GO_numbers_with_names2 <- setNames(GO_numbers_with_names2, c("id", "name", "ontology"))
+GO_numbers_with_names <- setNames(GO_numbers_with_names, c("id", "name", "ontology"))
 
 #write GO_numbers_with_names2 to a file
-write.table(GO_numbers_with_names2,"GO_numbers_with_names2.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(GO_numbers_with_names,"GO_numbers_with_names2.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 
 #change the colnames of gaf_filtered to match the GO_numbers_with_names2
 colnames(gaf_filtered) <- c("gene", "id", "ontology")
 
-# join the gaf_filtered and GO_numbers_with_names2 on id
-GO_names_and_genes <- dplyr::left_join(gaf_filtered, GO_numbers_with_names2, by = "id")
 
-GO_names_and_genes<-GO_names_and_genes |> dplyr::select(id,name,gene, ontology = ontology.x)
+
+
+# join the gaf_filtered and GO_numbers_with_names2 on id
+GO_names_and_genes <- dplyr::left_join(gaf_filtered, GO_numbers_with_names, by = "id")
+
+GO_names_and_genes<- GO_names_and_genes |> dplyr::select(id,name,gene, ontology = ontology.x)
+
+# show the number of unique GO terms
+length(unique(GO_names_and_genes$id))
+
+
 
 #save GO_names_and_genes to a file
 write.table(GO_names_and_genes,"GO_names_and_genes.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
@@ -102,6 +132,12 @@ str(To_parquet)
 
 # Write to Parquet file for Vivid-Volcano application 
 write_parquet(To_parquet, "GO.parquet2")
+
+
+
+
+############################## Estimation of n of level 3 hierarchy terms##########################
+
 
 # Estimate the number of non-redundant no overlapping GO categories at level 3
 
